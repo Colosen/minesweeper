a0537d33694390086d765559b162b3d02873ebfc
Colosen <jith.var.1@gmail.com>
2022-09-05 21:41:21 +0530

    Added some comments

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 import pygame
 
 pygame.init()
+
+# creating Pygame window
 WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Minesweeper")
@@ -10,21 +12,29 @@ BLUE = (128, 205, 224)
 LIGHT_BLUE = (209, 246, 255)
 GREY = (100, 100, 100)
 
+
 def main():
     run = True
-    while run:
-        for event in pygame.event.get():
+    while run: # loop during the time the window is open
+        for event in pygame.event.get(): # loop through events indefinitely
             if event.type == pygame.QUIT:
-                run = False
+                run = False # terminate program
 
         WIN.fill(BLUE)
-        for i in range(20, WIDTH-20, 46):
-            for j in range(20, HEIGHT-20, 46):
-                pygame.draw.rect(WIN, LIGHT_BLUE, rect=(i, j, 48, 48), border_radius=5)
-                pygame.draw.rect(WIN, GREY, rect=(i, j, 48, 48), width=2, border_radius=5)
+        create_squares()
         pygame.display.update()
 
     pygame.quit()
+
+
+def create_squares():
+    SIDE_LENGTH = 46
+    BORDER_RADIUS = 5
+    BORDER_WIDTH = 2
+    for i in range(20, WIDTH-20, 46): # draw 100 squares in 460x460 square
+        for j in range(20, HEIGHT-20, 46):
+            pygame.draw.rect(WIN, LIGHT_BLUE, rect=(i, j, SIDE_LENGTH, SIDE_LENGTH), border_radius=BORDER_RADIUS) # light blue squares
+            pygame.draw.rect(WIN, GREY, rect=(i, j, SIDE_LENGTH, SIDE_LENGTH), width=BORDER_WIDTH, border_radius=BORDER_RADIUS) # grey border around every square
 
 
 if __name__ == "main.py":
